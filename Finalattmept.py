fbc2704b2d685d7a3528612880ef7483175ebeb5
Coreym6 <cmcdani3@samford.edu>
2023-11-03 11:17:10 -0500

    edited the footer list, took out recompile, etc.

--- Finalattmept.py
+++ Finalattmept.py
@@ -1,7 +1,15 @@
 import re
 
 pdf_sig = re.compile(b'\x25\x50\x44\x46')
-pdf_footer = re.compile(b'\x0A\x25\x25\x45\x4F\x46\x0A')
+footer_patterns = [
+    b'\x0A\x25\x25\x45\x4F\x46\x0A',
+    b'\x0D\x0A\x25\x25\x45\x4F\x46\x0D\x0A',
+    b'\x0A\x25\x25\x45\x4F\x46\x0A',
+    b'\x0A\x25\x25\x45\x4F\x46'
+]
+
+pdf_footer_sig = re.compile(b'|'.join(footer_patterns))
+#pdf_footer = re.compile(b'\x0A\x25\x25\x45\x4F\x46\x0A')
 #b'\x0D\x0A\x25\x25\x45\x4F\x46\x0D\x0A',
 #b'\x0A\x25\x25\x45\x4F\x46\x0A',
 #b'\x0A\x25\x25\x45\x4F\x46') '''
@@ -13,7 +21,8 @@ def pdf_recov(meta_pdf, output_directory, headers_list, pdf_sig, pdf_footer):
     #changed the pattern to the header of an pdf
     #H = re.compile(header_sig)
     header_to_bytes = pdf_sig.encode('utf-8') 
-    footer_to_bytes = pdf_footer.encode('utf-8')
+    footer_to_bytes = pdf_footer_sig # might have to add an encoding if that doesn't work
+    #footer_to_bytes = pdf_footer.encode('utf-8')
     #footer_pattern = re.compile(b'\xFF\xD9') # this can be for the footer of the file
     ''' There is multiple footers, here is the file sigs for this ones,
     b'\x0A\x25\x25\x45\x4F\x46\x0A',
@@ -38,13 +47,13 @@ for match in pdf_sig.finditer(disk_image_data):
 print("List of matched offsets:", matched_offsets)
 #header_offsets = [match.start() for match in header_sig.finditer(disk_image_data)] # this is correct method,open diskimage.dd(head))
 
-matched_footer_offsets =[]
+matched_footer_offsets =[] # I might have to change this
 with open("Project2.dd", "rb") as file:
     disk_image_data2 = file.read()
 
 for match in pdf_sig.finditer(disk_image_data):
     offset = match.start()
-    matched_footer_offsets.append(offset)
+    matched_footer_offsets.append(offset) # and possibly this to another list
     print(f"Found footer pattern at offset: {offset}")
 print("List of matched ending offsets:", matched_footer_offsets)
 
