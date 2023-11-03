5ff3f2338291ca37f332e84bf5e95049cb562f05
Coreym6 <cmcdani3@samford.edu>
2023-11-03 18:24:56 -0500

    added bmp recov function

--- Finalattmept.py
+++ Finalattmept.py
@@ -68,5 +68,49 @@ with open(f"recovered_file_{carved_data[:20]}.pdf", "wb") as carved_file:
 #will fix later to the files, we at least got the offsets
 # also I need to add SHA-256 Function 
 
+bmp_sig = re.compile(b'\x42\x4D....\x00\x00\x00\x00')# might have to edit this
+bmp_footer_sig = re.compile(b'None') # I'll either have to feed that in or possible the file size
+# the file size for the bmp according to the disk editor is 77,942
+# start of the bmp file recover
+def bmp_recov( bmp_sig, pdf_footer_sig):
 
+    # Compile the regex pattern using the header signature
 
+    #changed the pattern to the header of an pdf
+    #H = re.compile(header_sig)
+    header_to_bytes = bmp_sig.encode('utf-8') 
+    footer_to_bytes = pdf_footer_sig.encode('utf-8') # might have to add an encoding if that doesn't work
+    #footer_to_bytes = pdf_footer.encode('utf-8')
+    #footer_pattern = re.compile(b'\xFF\xD9') # this can be for the footer of the file
+    ''' There is multiple footers, here is the file sigs for this ones,
+    b'\x0A\x25\x25\x45\x4F\x46\x0A',
+        b'\x0D\x0A\x25\x25\x45\x4F\x46\x0D\x0A',
+        b'\x0A\x25\x25\x45\x4F\x46\x0A',
+        b'\x0A\x25\x25\x45\x4F\x46
+        I will likely have to approach it in a similar manner'''
+    # Create an empty list to store matched offsets
+matched_offsets = []
+matched_footer_offsets =[]
+# Read the disk image file
+with open("Project2.dd", "rb") as file:
+    disk_image_data = file.read()
+
+# Search for header pattern and store matched offsets
+for match in bmp_sig.finditer(disk_image_data):
+    offset = match.start()
+    matched_offsets.append(offset)
+    print(f"Found header pattern at offset: {offset}")
+
+# Print the list of matched offsets
+print("List of matched offsets:", matched_offsets)
+#header_offsets = [match.start() for match in header_sig.finditer(disk_image_data)] # this is correct method,open diskimage.dd(head))
+
+matched_footer_offsets =[] # I might have to change this
+with open("Project2.dd", "rb") as file:
+    disk_image_data2 = file.read()
+
+for match in pdf_footer_sig.finditer(disk_image_data2): 
+    offset = match.start()
+    matched_footer_offsets.append(offset) # and possibly this to another list
+    print(f"Found footer pattern at offset: {offset}")
+print("List of matched ending offsets:", matched_footer_offsets)
